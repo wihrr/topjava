1446dcfb2b45c1223aaa4ada0815b8b13b3beb9a
Viktoria Vihrova <viktoria.vihrova@intexsoft.by>
2021-11-25 16:48:13 +0300

    ITX4OTHER-480 HW4 connect to new DB and set up this DB
    rename the table in populateDB.sql, initDB_hsql.sql and Meal.java. Change username and password for DB in postgres.properties

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -4,7 +4,6 @@ import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
@@ -18,7 +17,7 @@ import java.time.LocalTime;
         @NamedQuery(name = Meal.FOR_THE_PERIOD, query = "SELECT m FROM Meal m WHERE m.user.id=:userId  AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime ORDER BY m.dateTime DESC")
 })
 @Entity
-@Table(name = "meal")
+@Table(name = "meals")
 public class Meal extends AbstractBaseEntity {
 
     public static final String DELETE = "Meal.delete";
